694d7a246d603be045e71b78c9555a685a91b33b
Md.Amanullah Akon <79790568+amandiu@users.noreply.github.com>
2024-12-28 22:11:42 +0600

    success

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useEffect, useState } from "react";
+import{ useEffect, useState } from "react";
 
 const App = () => {
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
-
   return (
     <div>
-      <h1>Welcome to my User management system</h1>
-      <p>There are users numbers: {users.length} </p>
+      <h1>Welcome to my website</h1>
+      <p>There are users here for: {users.length}</p>
       {
         users.map(user =><p key={user.id}>{user.name}: {user.email}</p>)
       }
